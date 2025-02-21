bbc7fd62622570ebd0ad16f4097892b7c755de50
Sami Nafis <78338492+saminafisdev@users.noreply.github.com>
2025-02-22 02:46:56 +0600

    Refactor Home component layout and add curve line SVG for improved design

--- app/page.js
+++ app/page.js
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import carbon from "@/public/carbon.svg";
 import githubLogoWhite from "@/public/github-mark-white.svg";
+import curveLine from "@/public/curve_line.svg";
 import { Skills } from "./Skills";
 import { Projects } from "./Project";
 
 export default function Home() {
   return (
-    <div className="p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <section className="flex items-center justify-center gap-32 min-h-screen">
+    <div className="px-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <section className="flex flex-col md:flex-row items-center justify-center gap-6 md:gap-32 min-h-screen">
         <div>
-          <p className="text-indigo-700 text-lg font-semibold">
+          <p className="text-emerald-700 text-lg font-semibold">
             Hi, my name is
           </p>
-          <h3 className="text-8xl font-bold">Sami Nafis</h3>
+          <h3 className="text-5xl md:text-8xl font-bold">Sami Nafis</h3>
           <p className="mt-4 text-lg">Software Engineer 👨‍💻 | Student 👨‍🎓</p>
           <button className="mt-4 bg-black text-white flex items-center gap-3 px-4 py-2 rounded-md">
             <Image src={githubLogoWhite} alt="Github logo" height={30} />
@@ -21,7 +22,13 @@ export default function Home() {
             </a>
           </button>
         </div>
-        <Image src={carbon} alt="Carbon" width={800} />
+        <Image src={carbon} alt="Carbon" height={350} />
+        <Image
+          src={curveLine}
+          alt="Carbon"
+          width={800}
+          className="absolute top-0 right-0"
+        />
       </section>
       <Skills />
       <Projects />
